85bb958d0186a80193dd8c10a3188983e745418a
Alex Torres <alextcarvalho98@gmail.com>
2025-06-27 17:39:18 -0400

    Personalizando e mudando o questionário

--- app.py
+++ app.py
@@ -5,23 +5,23 @@ import io
 import base64
 from datetime import datetime
 
-st.set_page_config(page_title="Cadastro + QR Pix", layout="centered")
-st.title("Cadastro da Célula e Oferta via Pix")
+st.set_page_config(page_title="Inscrição ACAMP 2025", layout="centered")
+st.title("Inscrição ACAMP 2025")
 
 # --- Formulário ---
 nome = st.text_input("Nome completo")
 idade = st.number_input("Idade", min_value=0, max_value=120, step=1)
-igreja = st.radio("Qual igreja?", ["Sede", "Extensao", "Outra igreja"])
+igreja = st.radio("Qual igreja?", ["Sede (IDPB Redenção)", "Extensão", "Outra igreja"])
 linhagem = st.text_input("Linhagem")
 tempo_convertido = st.text_input("Tempo de convertido")
 
 st.markdown("**Esportes (ordem de habilidade)**")
-esporte1 = st.selectbox("1º Esporte", ["", "Futebol", "Vôlei", "Natação", "Tênis de mesa"])
-esporte2 = st.selectbox("2º Esporte", ["", "Futebol", "Vôlei", "Natação", "Tênis de mesa"])
-esporte3 = st.selectbox("3º Esporte", ["", "Futebol", "Vôlei", "Natação", "Tênis de mesa"])
+esporte1 = st.selectbox("1º Esporte", ["", "Futebol", "Vôlei", "Natação", "Tênis de mesa", "Corrida"])
+esporte2 = st.selectbox("2º Esporte", ["", "Futebol", "Vôlei", "Natação", "Tênis de mesa", "Corrida"])
+esporte3 = st.selectbox("3º Esporte", ["", "Futebol", "Vôlei", "Natação", "Tênis de mesa", "Corrida"])
 
-conhecimento_midia = st.radio("Conhecimento em Mídia?", ["Sim", "Não"])
-quiz = st.radio("Quiz 'Sou melhor' em:", ["Conhecimentos Gerais", "Conhecimento Bíblico", "Nenhuma das alternativas"])
+conhecimento_midia = st.radio("Possui conhecimento em Mídia?", ["Sim, em social media", "Sim, em gravação e edição de vídeos", "Não"])
+quiz = st.radio("Em quiz, sou melhor em:", ["Conhecimentos Gerais", "Conhecimento Bíblico", "Nenhuma das alternativas"])
 
 # --- Processamento ---
 if st.button("Enviar e Gerar Pix"):
@@ -54,7 +54,7 @@ if st.button("Enviar e Gerar Pix"):
 
         # Geração do QR Pix
         chave_pix = "sua-chave@pix.com"
-        nome_loja = "Igreja Local"
+        nome_loja = "IGREJA DE DEUS PENTECOSTAL DO BRASIL"
         cidade = "Manaus"
         valor = 10.00  # Valor fixo para o exemplo
 
